76b3724c1226c2743f5613e65a53c6d6c59d2522
Genghis Bai <bai.genghis@gmail.com>
2016-12-04 11:48:08 -0800

    modified footer

--- wp-content/themes/wptheme/footer.php
+++ wp-content/themes/wptheme/footer.php
@@ -1,5 +1,9 @@
 	<!--site-footer -->
 	<footer class="site-footer">
+	<!--site-nav -->
+	<nav class="site-nav">
+		<?php wp_nav_menu(); ?>
+	</nav><!-- /site-nav -->
 		<p><?php bloginfo('name'); ?> - &copy; <?php echo date('Y'); ?></p>
 	</footer><!-- /site-footer -->
 </div><!-- /container -->
